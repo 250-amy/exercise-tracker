2a6020bfa84ab6d037f75f7ae397989dbd399e09
250-amy <amy.tencer@gmail.com>
2022-10-14 11:52:21 -0700

    remove unnecessary import from main js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
 import './index.css'
-import VueSimpleSuggest from 'vue-simple-suggest'
 
 createApp(App).mount('#app')
