d27ff2602acdcc85d60db3f4d5d627585d697609
250-amy <amy.tencer@gmail.com>
2022-10-14 11:12:02 -0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './style.css'
+import './index.css'
+import VueSimpleSuggest from 'vue-simple-suggest'
+
+createApp(App).mount('#app')
